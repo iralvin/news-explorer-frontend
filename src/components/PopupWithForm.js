c95eb013c271c689270b8fefa96a6b88698dbd4c
alvin <alvinkwong1@gmail.com>
2020-12-02 00:37:40 -0500

    started popup template

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function PopupWithForm(){
-    return (
-        <div className="popup section">
-            <div className="popup__dialog-window">
-
-            </div>
-        </div>
-    )
+function PopupWithForm() {
+  return (
+    <div className="popup section">
+      <div className="popup__dialog-window">
+        <form noValidate autoComplete="off">
+            <h2 className="popup__title">Sign Up</h2>
+        </form>
+      </div>
+    </div>
+  );
 }
 export default PopupWithForm;
