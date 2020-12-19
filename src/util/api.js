f153cc11c49c152d08c1113251bd3243e2c97149
alvin <alvinkwong1@gmail.com>
2020-12-19 13:38:04 -0500

    added api to get user saved articles

--- src/util/api.js
+++ src/util/api.js
@@ -1,17 +1,32 @@
+const newsApiKey = "66c76d6a626b4e74a20ef60d3c03128a";
+
 class Api {
-    constructor(baseUrl){
-        this._baseUrl = baseUrl;
-    }
+  constructor(baseUrl) {
+    this._baseUrl = baseUrl;
+  }
 
+  getSavedArticles(token, user) {
+    return fetch(`${this._baseUrl}articles/${user._id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    });
+  }
 
-    // GET user data after logged in
-    // GET user saved articles
-    
-    // POST saved article to user data
+  // GET user data after logged in
+  // GET user saved articles
 
-    // GET news articles
+  // POST saved article to user data
 
-
-
-
+  // GET news articles
 }
+
+const api = new Api("https://www.api.alvin.students.nomoreparties.site/");
+
+export default api;
