e7b4bcbf0923d7b51a65b9db0284becdfab5fc27
alvin <alvinkwong1@gmail.com>
2020-12-24 17:52:56 -0500

    added pagination function with show more button

--- src/components/NewsCardsList/NewsCardsList.js
+++ src/components/NewsCardsList/NewsCardsList.js
@@ -1,35 +1,48 @@
-import React from 'react';
-import NewsCard from '../NewsCard/NewsCard';
+import React from "react";
+import NewsCard from "../NewsCard/NewsCard";
 
 function NewsCardsList(props) {
   const [displayedArticleCards, setDisplayedArticleCards] = React.useState([]);
   const [showMoreButtonDisabled, setShowMoreButtonDisabled] = React.useState(
     false
   );
+  const [pageCount, setPageCount] = React.useState(1);
 
+  const pageSize = 3;
+
+  // function to show an additional 3 articles
   function showMoreArticles() {
-    setDisplayedArticleCards(props.data);
-    setShowMoreButtonDisabled(true);
+    const tempPageCount = pageCount;
+    setPageCount(tempPageCount + 1);
   }
 
   function displayThreeArticles() {
-    setDisplayedArticleCards(props.data.slice(0, 3));
+    setDisplayedArticleCards(props.data.slice(0, pageSize));
   }
 
   React.useEffect(() => {
-    // setDisplayedArticleCards(props.data);
-
     displayThreeArticles();
     if (props.data.length > 3) {
       setShowMoreButtonDisabled(false);
     }
   }, [props.data]);
 
+  React.useEffect(() => {
+    console.log("current pagesize * pagecount = ", pageSize * pageCount);
+    if (pageSize * pageCount > props.data.length) {
+      console.log("reached end of search");
+      setDisplayedArticleCards(props.data);
+      setShowMoreButtonDisabled(true);
+    } else {
+      setDisplayedArticleCards(props.data.slice(0, pageSize * pageCount));
+    }
+  }, [pageCount]);
+
   return (
-    <div className='news-list section'>
-      <div className='news-list__container'>
-        <h2 className='news-list__title'>Search results</h2>
-        <ul className='news-list__cards-list'>
+    <div className="news-list section">
+      <div className="news-list__container">
+        <h2 className="news-list__title">Search results</h2>
+        <ul className="news-list__cards-list">
           {displayedArticleCards.map((article, index) => {
             return (
               <NewsCard
@@ -60,7 +73,7 @@ function NewsCardsList(props) {
 
         <button
           className={`news-list__show-more-button ${
-            showMoreButtonDisabled ? 'hidden' : ''
+            showMoreButtonDisabled ? "hidden" : ""
           }`}
           onClick={showMoreArticles}
         >
