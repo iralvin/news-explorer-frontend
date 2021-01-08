d49a40297a0a191c49f7f07e4f24e3fa0c48ded0
alvin <alvinkwong1@gmail.com>
2021-01-08 00:19:33 -0500

    deploy build

--- src/util/api.js
+++ src/util/api.js
@@ -8,15 +8,15 @@ class Api {
     this._currentDate = new Date(Date.now()).toISOString();
     this._baseUrl = 'https://api.y-alvin.students.nomoreparties.site/';
     this._newsTopUrl = 'https://newsapi.org/v2/top-headlines?';
-    this._newsSearchUrl = `https://nomoreparties.co/news/v2/everything?language=en&pageSize=100&sortBy=publishedAt&from=${this._searchFromDate}&to=${this._currentDate}&`;
+    this._newsSearchUrl = `https://nomoreparties.co/news/v2/everything?language=en&pageSize=100&sortBy=publishedAt&from=${this._searchFromDate}&to=${this._currentDate}&apiKey=${apiKey}`;
   }
 
   getNewsSearchedArticles(query) {
     return fetch(`${this._newsSearchUrl}q=${query}`, {
       method: 'GET',
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      }
+      // headers: {
+      //   Authorization: `Bearer ${apiKey}`,
+      // }
     }).then((res) => {
       if (res.ok) {
         return res.json();
