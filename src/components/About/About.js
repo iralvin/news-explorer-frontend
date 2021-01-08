b1b8854c63b4e4de6fa98a1a29cd5bb1b2374b3a
Alvin <50558187+iralvin@users.noreply.github.com>
2021-01-08 14:41:16 -0500

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,12 +8,10 @@ function About(props) {
         <div className='about__text-container'>
           <p className='about__text about__text_subtitle'>About the author</p>
           <p className='about__text about__text_description'>
-            This block describes the project author. Here you should indicate
-            your name, what you do, and which development technologies you know.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Metus dictum at tempor commodo ullamcorper a lacus. 
             <br/>
             <br/>
-            You can also talk about your experience with Practicum, what you
-            learned there, and how you can help potential customers.
+            Amet nisl purus in mollis nunc. Pellentesque nec nam aliquam sem et. Id porta nibh venenatis cras sed felis eget velit. Sit amet luctus venenatis lectus magna fringilla urna.
           </p>
         </div>
       </div>
